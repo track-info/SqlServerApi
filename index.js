fdd0336739420d657c93f0c39b003210e56e9276
trackinfo.ig@gmail.com <trackinfo.ig@gmail.com>
2025-03-07 10:49:25 -0300

    atualizacao endpoint buscar cliente

--- index.js
+++ index.js
@@ -161,7 +161,7 @@ app.get("/cliente/:celular", async (req, res) => {
       Validade: cliente.Validade
     };
 
-    const message = `Cliente encontrado com sucesso! Nome: ${clienteData.Nome}, Celular: ${clienteData.Celular}, CPF: ${clienteData.CPF}, Email: ${clienteData.Email}, Assinante: ${clienteData.Assinante}, Pagamento em Dia: ${clienteData.PagtoEmDia}, Preferência de Resposta: ${clienteData.PrefResp}, Saldo de Mensagens em Texto: ${cliente.SaldoTrocaMensTexto}, Saldo de Mensagens em Audio: ${cliente.SaldoTrocaMensAudio}, Validade de Mensagens: ${cliente.Validade}`;
+    const message = `Cliente encontrado com sucesso! Nome: ${clienteData.Nome}, Celular: ${clienteData.Celular}, CPF: ${clienteData.CPF}, Email: ${clienteData.Email}`;
 
     res.status(200).json({
       message: message,
